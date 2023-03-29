ac06f6d48221c2014df1d7aebab7b6a5baee0c40
Agryo Araujo <agryostallion@gmail.com>
2023-03-29 01:09:57 -0300

    Continuação

--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -4,18 +4,11 @@ public class Curso extends Conteudo{
     private int cargaHoraria;
 
     @Override
-    public double calcularXp() {
-        return XP_PADRAO * cargaHoraria;
-    }
-    public Curso() {
-    }
+    public double calcularXp() { return XP_PADRAO * cargaHoraria; }
+    public Curso() {}
 
-    public int getCargaHoraria() {
-        return cargaHoraria;
-    }
-    public void setCargaHoraria(int cargaHoraria) {
-        this.cargaHoraria = cargaHoraria;
-    }
+    public int getCargaHoraria() { return cargaHoraria; }
+    public void setCargaHoraria(int cargaHoraria) { this.cargaHoraria = cargaHoraria; }
     @Override
     public String toString() {
         return "Curso{" +
